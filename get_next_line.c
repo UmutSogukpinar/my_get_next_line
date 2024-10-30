aa022bd1cbb9e4aaffada3792770619cacbc3ef4
UmutSogukpinar <usogukpinar0@gmail.com>
2024-10-30 11:14:56 +0300

    trunc finished some problems remain

--- get_next_line.c
+++ get_next_line.c
@@ -19,21 +19,34 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int	ft_set_initial_data(char *data)
+int	ft_set_data(char **data)  // func1
+{
+	*data = (char *) malloc(sizeof(char));
+	if (*data)
+		**data = '\0';
+	return (*data == NULL);
+}
+
+int	ft_rearrange_remains(char *remains, char *pos)
 {
 	int	i;
+	int	j;
 
-	i = 0;
-	data = malloc(BUFFER_SIZE * sizeof(char));
-	while (data[i] != '\0')
+	if (!pos)
+		return (-1);
+	i = 1;
+	j = 0;
+	while(pos[i] != '\0')
 	{
-		data[i] = 0;
+		remains[j] = pos[i];
 		i++;
+		j++;
 	}
+	remains[j] = '\0';
 	return (1);
 }
 
-char	*ft_create_and_free_arr(char *old, char *buffer)
+char	*ft_create_and_free_arr(char *old, char *buffer)  // func2
 {
 	char 	*new;
 	char	*new_buffer;
@@ -48,9 +61,9 @@ char	*ft_create_and_free_arr(char *old, char *buffer)
 	else
 		total_len = old_str_len + buffer_len + 1;
 	new = malloc(sizeof(char) * (total_len));
-	if (!new || !ft_set_initial_data(new))
+	if (!new || ft_set_data(new))
 		return (NULL);
-	if (buffer[BUFFER_SIZE] == '\n')
+	if (buffer[buffer_len] == '\n')
 		new_buffer = ft_substr(buffer, 0, buffer_len + 1);
 	else
 		new_buffer = ft_substr(buffer, 0, buffer_len);
@@ -60,16 +73,31 @@ char	*ft_create_and_free_arr(char *old, char *buffer)
 	return (new);
 }
 
-// char	*get_next_line(int fd)
-// {
-// 	char		buffer[BUFFER_SIZE + 1];
-// 	char		*total_line;
-// 	static char *remaining_data = 0;
+char	*get_next_line(int fd) // func3
+{
+	char		buffer[BUFFER_SIZE + 1];
+	char		*total_line;
+	char		*position;
+	static char *remains = 0;
+	int			bytes_read;
 
-// 	if (fd < 0 || ft_set_initial_data(remaining_data))
-// 		return (NULL);
-	
-// }
+	if ((fd < 0 && remains) || ft_set_data(remains))
+		return (NULL);
+	ft_set_data(total_line);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	buffer[bytes_read] = '\0';
+	position = ft_strchr(buffer, '\n');
+	while (!position && bytes_read != 0)
+	{
+		total_line = ft_create_and_free_arr(total_line, buffer);
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		buffer[bytes_read] = '\0';
+		position = ft_strchr(buffer, '\n');
+	}
+	if (position)
+		ft_rearrange_remains(remains, position);
+	return (total_line);
+}
 /*deleted part*/
 
 char	*ft_strdup(const char *s1)
@@ -94,10 +122,10 @@ char	*ft_strdup(const char *s1)
 
 int main()
 {
-	char *s = ft_strdup("ahmet ");
-	char *buffer = ft_strdup("buffer\n 1 4234");
+	int fd = open("deneme.txt", O_RDWR, 0666);
 
-	char *new = ft_create_and_free_arr(s, buffer);
-	// printf("new: %s", new);
-	printf("buf: %s", buffer);
+	char *result = get_next_line(fd);
+	printf("%s\n", result);
+
+	close(fd);
 }
